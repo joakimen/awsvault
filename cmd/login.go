8445b48b1e7ee2bb5562d424313e54235fa7e178
Joakim L. Engeset <joakim.engeset@gmail.com>
2024-09-15 13:21:53 +0200

    feat: switch to firefox with container tabs
    Chrome may still be used via --browser/-b "chrome"

--- cmd/login.go
+++ cmd/login.go
@@ -5,9 +5,16 @@ import (
 	"github.com/joakimen/awsvault/pkg/aws"
 	"github.com/joakimen/awsvault/pkg/fs"
 	"github.com/spf13/cobra"
+	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
+)
+
+const (
+	chromePath  string = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+	firefoxPath string = "/Applications/Firefox.app/Contents/MacOS/firefox"
 )
 
 // loginCmd represents the login command
@@ -15,13 +22,17 @@ var loginCmd = &cobra.Command{
 	Use:   "login",
 	Short: "Open a browser window and login using temporary credentials",
 	Run: func(cmd *cobra.Command, args []string) {
-		loginFn()
+		loginFn(cmd)
 	},
 }
 
-func loginFn() {
+func loginFn(cmd *cobra.Command) {
 
-	var chromePath = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+	browser, _ := cmd.Flags().GetString("browser")
+	if browser != "chrome" && browser != "firefox" {
+		fmt.Fprintf(os.Stderr, "Invalid browser: %s\n", browser)
+		os.Exit(1)
+	}
 
 	conf, err := aws.ReadConfig()
 	if err != nil {
@@ -46,23 +57,48 @@ func loginFn() {
 	}
 
 	loginLink := string(output)
-	xdgDataDir := filepath.Join(fs.XDGDataDir(), "aws_chrome", profile.Name)
-	xdgCacheDir := filepath.Join(fs.XDGCacheDir(), "aws_chrome", profile.Name)
 
-	chrome := exec.Command(chromePath,
-		"--no-first-run",
-		"--start-maximized",
-		"--user-data-dir="+xdgDataDir,
-		"--disk-cache-dir="+xdgCacheDir,
-		loginLink)
+	if browser == "chrome" {
+		xdgDataDir := filepath.Join(fs.XDGDataDir(), "aws_chrome", profile.Name)
+		xdgCacheDir := filepath.Join(fs.XDGCacheDir(), "aws_chrome", profile.Name)
 
-	err = chrome.Start()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error starting browser: %v\n", err)
-		os.Exit(1)
+		chrome := exec.Command(chromePath,
+			"--no-first-run",
+			"--start-maximized",
+			"--user-data-dir="+xdgDataDir,
+			"--disk-cache-dir="+xdgCacheDir,
+			loginLink)
+
+		err = chrome.Start()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error starting browser '%s': %v\n", browser, err)
+			os.Exit(1)
+		}
+
+	} else {
+		// need this before passing link to tab container extension
+		urlEncodedLoginLink := url.QueryEscape(loginLink)
+
+		var groupColor string
+		if strings.Contains(profile.Name, "-prod") {
+			groupColor = "red"
+		} else if strings.Contains(profile.Name, "-staging") {
+			groupColor = "yellow"
+		} else {
+			groupColor = "green"
+		}
+
+		containerTabCommand := fmt.Sprintf("ext+container:name=%s&color=%s&url=%s", profile.Name, groupColor, urlEncodedLoginLink)
+		firefox := exec.Command(firefoxPath, containerTabCommand)
+		err = firefox.Start()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error starting browser '%s': %v\n", browser, err)
+			os.Exit(1)
+		}
 	}
 }
 
 func init() {
 	rootCmd.AddCommand(loginCmd)
+	loginCmd.Flags().StringP("browser", "b", "firefox", "Browser to use (chrome, firefox, default)")
 }
